a6036b887e8115e2289fcb3475f64e2565ddcf8b
Mikeisnotakid <teekaamichaele@gmail.com>
2024-06-28 17:02:21 +0100

    javascript

---
+++ Assignment/babcock.js
@@ -0,0 +1,27 @@
+let SearchBtn = document.querySelector(".fa-bars");
+
+let Cancel_icon = document.querySelector('.fa-times');
+
+
+
+SearchBtn.addEventListener("click", function () {
+    document.querySelector(".nav-dropdown-div").style.display = "block";
+
+    SearchBtn = document.querySelector(".fa-bars").style.display = "none";
+    Cancel_icon = document.querySelector(".fa-times").style.display = "block";
+
+
+})
+
+Cancel_icon.addEventListener("click", function () {
+    document.querySelector(".nav-dropdown-div").style.display = "none";
+    SearchBtn = document.querySelector(".fa-bars").style.display = "block";
+    Cancel_icon = document.querySelector(".fa-times").style.display = "none";
+});
+
+
+
+
+
+
+
