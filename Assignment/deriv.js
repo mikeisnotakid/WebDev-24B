ac63ad01e26bb01ce9abbe84db68b71999fe201a
Mikeisnotakid <teekaamichaele@gmail.com>
2024-07-14 19:43:43 +0100

    drop down added

---
+++ Assignment/deriv.js
@@ -0,0 +1,10 @@
+// var DerivedList = document.getElementById('Derived-indicesLi')
+// var DerivedIndices = document.querySelector('.derived-section')
+// var ForexSection = document.querySelector('.Forex-section')
+
+
+// DerivedList.addEventListener('click', function(){
+
+//    DerivedIndices.style.display = 'block';
+//    ForexSection.style.display = 'none'
+// })
