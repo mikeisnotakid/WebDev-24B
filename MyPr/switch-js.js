2a3a5057f67e5b7fc06910ea889449c59a0b163d
Mikeisnotakid <teekaamichaele@gmail.com>
2024-07-20 17:31:51 +0100

    wqadefsdg

---
+++ MyPr/switch-js.js
@@ -0,0 +1,28 @@
+function Container() {
+    document.querySelector('.container').style.display = 'block';
+    document.querySelector('.container3').style.display = 'none';
+    document.querySelector('.container4').style.display = 'none';
+    document.querySelector('.container2').style.display = 'none';
+
+
+}
+function Container2() {
+    document.querySelector('.container2').style.display = 'block';
+    document.querySelector('.container3').style.display = 'none';
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container4').style.display = 'none';
+}
+
+function Container3() {
+    document.querySelector('.container3').style.display = 'block';
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container2').style.display = 'none';
+    document.querySelector('.container4').style.display = 'none';
+}
+
+function Container4() {
+    document.querySelector('.container4').style.display = 'block';
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container2').style.display = 'none';
+    document.querySelector('.container3').style.display = 'none';
+}
