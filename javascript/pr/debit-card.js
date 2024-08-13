cae49e6f1dcc2351734edbbd767a716c88de7595
Mikeisnotakid <teekaamichaele@gmail.com>
2024-08-13 16:07:03 +0100

    ytu

--- javascript/pr/debit-card.js
+++ javascript/pr/debit-card.js
@@ -1,15 +1,25 @@
 var CardNumber = document.getElementById('cardNumber');
-var RecieveCardNum =  document.getElementById('RecieveCardNumber');
+var RecieveCardNum = document.getElementById('RecieveCardNumber');
+var CardNAme = document.getElementById('cardName');
+var CardMM = document.getElementById('MM');
+var CardYY = document.getElementById('YY');
+var CardCVC = document.getElementById('CVC');
+
 
 function SendCardName() {
-    var CardNAme = document.getElementById('cardName').value;
-    document.getElementById('RecieveCardName').innerHTML = CardNAme.toUpperCase();
-
+   
+    document.getElementById('RecieveCardName').innerHTML = CardNAme.value.toUpperCase();
+    if (CardNAme.value.length > 18) {
+        CardNAme.value = CardNAme.value.slice(0, 18)
+    }
 }
 function SendCardNumber() {
-    
-   RecieveCardNum.innerHTML = CardNumber.value;
-  
+    if (CardNumber.value.length > 14) {
+        CardNumber.value = CardNumber.value.slice(0, 14)
+    }
+
+    RecieveCardNum.innerHTML = CardNumber.value;
+
 }
 
 
@@ -17,19 +27,27 @@ function SendCardNumber() {
 
 
 function SendCardMM() {
-    var CardMM = document.getElementById('MM').value;
-    document.getElementById('CardMM').innerHTML = CardMM;
+    document.getElementById('CardMM').innerHTML = CardMM.value;
+    if (CardMM.value.length > 2){
+        CardMM.value = CardMM.value.slice(0,1)
+    }
 }
 
 function SendCardYY() {
-    var CardYY = document.getElementById('YY').value;
-    document.getElementById('CardYY').innerHTML = CardYY;
+    document.getElementById('CardYY').innerHTML = CardYY.value;
+    if(CardYY.value.length > 2){
+        CardYY.value = CardYY.value.slice(0,1)
+    }
 }
 
 
 function SendCardCVC() {
-    var CardCVC = document.getElementById('CVC').value;
-    document.getElementById('RecieveCardCVC').innerHTML = CardCVC;
+ 
+    document.getElementById('RecieveCardCVC').innerHTML = CardCVC.value;
+
+    if(CardCVC.value.length >3){
+        CardCVC.value = CardCVC.value.slice(0,2)
+    }
 
 }
 
